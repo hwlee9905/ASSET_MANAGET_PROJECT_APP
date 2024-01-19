83b34c6b0526bf046426379517205db97f08f085
hwlee9905 <9905hyun@naver.com>
2024-01-19 13:41:42 +0900

    project update

--- src/main/java/org/example/appproject_be/AppProjectBeApplication.java
+++ src/main/java/org/example/appproject_be/AppProjectBeApplication.java
@@ -1,8 +1,11 @@
 package org.example.appproject_be;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "https://assetmng-hwlee.koyeb.app/")})
 @SpringBootApplication
 public class AppProjectBeApplication {
 
