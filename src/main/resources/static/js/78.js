ccd791c4b4b7d1e018281c8eb897c3e08c3078ee
seoyeon <ryd10227@gmail.com>
2024-01-22 03:14:26 +0900

    front

--- src/main/resources/static/js/78.js
+++ src/main/resources/static/js/78.js
@@ -1,6 +1,6 @@
-"use strict";(self["webpackChunkams"]=self["webpackChunkams"]||[]).push([[78],{6078:function(e,t,n){n.r(t),n.d(t,{startStatusTap:function(){return a}});var s=n(65),r=n(8487),o=n(6587);
+"use strict";(self["webpackChunkbizMOB4_base_publishing"]=self["webpackChunkbizMOB4_base_publishing"]||[]).push([[78],{6078:function(e,t,n){n.r(t),n.d(t,{startStatusTap:function(){return i}});var s=n(65),o=n(6587);
 /*!
  * (C) Ionic http://ionicframework.com - MIT License
  */
-const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,s.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const i=(0,r.a)(a);i&&new Promise((e=>(0,o.c)(i,e))).then((()=>{(0,s.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,r.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+const i=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,s.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,i=document.elementFromPoint(t/2,n/2);if(!i)return;const r=i.closest("ion-content");r&&new Promise((e=>(0,o.c)(r,e))).then((()=>{(0,s.Iu)((async()=>{r.style.setProperty("--overflow","hidden"),await r.scrollToTop(300),r.style.removeProperty("--overflow")}))}))}))}))}}}]);
 //# sourceMappingURL=78.js.map
