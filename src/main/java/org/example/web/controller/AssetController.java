fa6d0520c3f24b02866a5185180c422c29f1e703
hwlee9905 <9905hyun@naver.com>
2024-02-03 21:04:41 +0900

    commit

--- src/main/java/org/example/web/controller/AssetController.java
+++ src/main/java/org/example/web/controller/AssetController.java
@@ -15,7 +15,6 @@ import java.util.List;
 @Slf4j
 public class AssetController {
     private final AssetService assetService;
-    //localhost:8080/assets
 
     @GetMapping("/assets/entire") //@RequestMapping(value = "/assets", method = RequestMethod.GET)
     public List<AssetDto> getAssets(){
